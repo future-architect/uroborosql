2ce5f940ab283f80268703b206fde2773b7a4ae6
sugimoto <h.sugimoto.j2@future.co.jp>
2017-04-17 02:18:20 +0900

    REPL improved! Implements code completion.

--- src/main/java/jp/co/future/uroborosql/client/SqlREPL.java
+++ src/main/java/jp/co/future/uroborosql/client/SqlREPL.java
@@ -30,12 +30,12 @@ import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import jline.console.ConsoleReader;
 import jline.console.UserInterruptException;
 import jline.console.completer.CandidateListCompletionHandler;
 import jline.console.completer.Completer;
-import jline.console.completer.StringsCompleter;
 import jp.co.future.uroborosql.SqlAgent;
 import jp.co.future.uroborosql.config.DefaultSqlConfig;
 import jp.co.future.uroborosql.config.SqlConfig;
@@ -76,16 +76,61 @@ public class SqlREPL {
 
 	/** コマンド名 */
 	private enum Command {
-		query(true), update(true), list(false), reload(false), driver(false), help(false), cls(false), exit(false);
+		Query(true, false), Update(true, false), List(true, false), Reload(false, false), Driver(false, false), Help(
+				false, false), Cls(false, false), Exit(false, false), ThisCmd(false, true);
 
 		private boolean hasArgs = false;
+		private boolean hidden = false;
 
-		private Command(final boolean hasArgs) {
+		private Command(final boolean hasArgs, final boolean hidden) {
 			this.hasArgs = hasArgs;
+			this.hidden = hidden;
 		}
 
+		/**
+		 * 引数を持つかどうか
+		 * @return 引数がある場合に<code>true</code>
+		 */
 		public boolean hasArgs() {
 			return hasArgs;
+		}
+
+		/**
+		 * 非表示コマンドかどうか
+		 * @return 非表示コマンドの場合<code>true</code>
+		 */
+		public boolean isHidden() {
+			return hidden;
+		}
+
+		/**
+		 * 指定された入力がコマンドにマッチするかどうかを判定する
+		 * @param input 入力文字列
+		 * @return コマンドにマッチする場合<code>true</code>
+		 */
+		public boolean match(final String input) {
+			return !isHidden() && toString().startsWith(input.toLowerCase());
+		}
+
+		@Override
+		public String toString() {
+			return super.toString().replace("Cmd", "").toLowerCase();
+		}
+
+		/**
+		 * 文字列からCommandを取得する
+		 * @param cmd コマンド文字列
+		 * @return 対応するCommandクラス。該当するCommandがない場合、HelpCommandを返却する
+		 */
+		public static Command toCommand(final String cmd) {
+			if (StringUtils.isNotBlank(cmd)) {
+				for (Command command : values()) {
+					if (command.toString().equalsIgnoreCase(cmd.trim())) {
+						return command;
+					}
+				}
+			}
+			return Help;
 		}
 	}
 
@@ -238,14 +283,20 @@ public class SqlREPL {
 		return props.getProperty(key, defaultValue);
 	}
 
+	@SuppressWarnings("resource")
 	private void listen() throws IOException {
 		ConsoleReader console = new ConsoleReader();
 		console.setPrompt("uroborosql > ");
 		console.setHandleUserInterrupt(true);
 
 		// コマンドのコード補完
-		console.addCompleter(new StringsCompleter(Arrays.asList(Command.values()).stream().map(c -> c.toString())
-				.collect(Collectors.toList())));
+		console.addCompleter((buffer, cursor, candidates) -> {
+			String buff = buffer.trim();
+			if (buff.length() > 0 && !buffer.endsWith(" ") || buff.isEmpty()) {
+				Stream.of(Command.values()).filter(c -> c.match(buff)).forEach(c -> candidates.add(c.toString()));
+			}
+			return candidates.isEmpty() ? -1 : 0;
+		});
 		// SQL名のコード補完
 		console.addCompleter(new SqlNameCompleter(config.getSqlManager().getSqlPathList()));
 		// バインドパラメータのコード補完
@@ -261,6 +312,7 @@ public class SqlREPL {
 					break;
 				}
 			} catch (UserInterruptException uie) {
+				break;
 			} catch (Exception e) {
 				e.printStackTrace(System.err);
 			}
@@ -281,15 +333,15 @@ public class SqlREPL {
 			return true;
 		}
 
-		String[] parts = line.split(" ");
-		String command = parts[0].toUpperCase();
+		String[] parts = getLineParts(line);
+		Command command = Command.toCommand(parts[0]);
 
 		switch (command) {
-		case "EXIT":
+		case Exit:
 			System.out.println("SQL REPL end.");
 			return false;
 
-		case "RELOAD":
+		case Reload:
 			System.out.println("RELOAD " + propPath);
 
 			for (Enumeration<Driver> drivers = DriverManager.getDrivers(); drivers.hasMoreElements();) {
@@ -304,18 +356,21 @@ public class SqlREPL {
 
 			return true;
 
-		case "LIST":
+		case List:
 			System.out.println("LIST:");
-			int sqlCount = 0;
-			List<String> pathList = config.getSqlManager().getSqlPathList();
-			int numSize = String.valueOf(pathList.size()).length();
-			for (String key : pathList) {
-				System.out.printf("%0" + numSize + "d : %s%n", ++sqlCount, key);
+			List<String> pathList = null;
+			if (parts.length > 1) {
+				pathList = config.getSqlManager().getSqlPathList().stream().filter(p -> p.contains(parts[1]))
+						.collect(Collectors.toList());
+			} else {
+				pathList = config.getSqlManager().getSqlPathList();
 			}
-
+			for (String key : pathList) {
+				System.out.printf("%s%n", key);
+			}
 			return true;
 
-		case "DRIVER":
+		case Driver:
 			System.out.println("DRIVER:");
 			Enumeration<Driver> drivers = DriverManager.getDrivers();
 			int driverCount = 0;
@@ -326,22 +381,22 @@ public class SqlREPL {
 
 			return true;
 
-		case "HELP":
+		case Help:
 			System.out.println("HELP:");
 			showMessage("/message.txt");
 
 			return true;
 
-		case "THIS":
+		case ThisCmd:
 			showMessage("/this.txt");
 
 			return true;
 
-		case "CLS":
+		case Cls:
 			console.clearScreen();
 			return true;
 
-		case "QUERY":
+		case Query:
 			if (parts.length >= 2) {
 				String sqlName = parts[1];
 				if (config.getSqlManager().existSql(sqlName)) {
@@ -349,7 +404,7 @@ public class SqlREPL {
 					try (SqlAgent agent = config.createAgent()) {
 						SqlContext ctx = agent.contextFrom(sqlName.replaceAll("\\.", "/"));
 						String[] params = Arrays.copyOfRange(parts, 2, parts.length);
-						setSqlParams(console, ctx, params);
+						setSqlParams(ctx, params);
 
 						ctx.setResultSetType(ResultSet.TYPE_SCROLL_INSENSITIVE);
 
@@ -365,7 +420,7 @@ public class SqlREPL {
 			}
 			return true;
 
-		case "UPDATE":
+		case Update:
 			if (parts.length >= 2) {
 				String sqlName = parts[1];
 				if (config.getSqlManager().existSql(sqlName)) {
@@ -373,7 +428,7 @@ public class SqlREPL {
 					try (SqlAgent agent = config.createAgent()) {
 						SqlContext ctx = agent.contextFrom(sqlName.replaceAll("\\.", "/"));
 						String[] params = Arrays.copyOfRange(parts, 2, parts.length);
-						setSqlParams(console, ctx, params);
+						setSqlParams(ctx, params);
 						try {
 							int ans = agent.update(ctx);
 							agent.commit();
@@ -394,31 +449,64 @@ public class SqlREPL {
 	}
 
 	/**
-	 * @param console
-	 * @param ctx
-	 * @param paramsArray
-	 * @throws IOException
+	 * 入力行をパーツに分解する。空白で分解した後に条件によって結合を行う。<br>
+	 *
+	 * 【結合条件】<br>
+	 * ・''で囲まれる値には空白を含めることができる。ex) 'String value includes whitespace.'<br>
+	 * ・[]で囲まれる値は配列を表す。 ex) [10, 20, 30, 40], ['aaa' , 'bbb' , 'ccc']
+	 *
+	 * @param line 入力行
+	 * @return パーツに分解した文字列配列
 	 */
-	private void setSqlParams(final ConsoleReader console, final SqlContext ctx, final String[] paramsArray)
-			throws IOException {
-		if (paramsArray.length > 0) {
-			for (String element : paramsArray) {
-				String[] param = element.split("=");
-				String key = param[0];
-				if (param.length == 1) {
-					// キーだけの指定は値をnullと扱う
-					ctx.param(key, null);
-				} else {
-					String val = param[1];
-					setParam(ctx, key, val);
+	private String[] getLineParts(final String line) {
+		String[] parts = line.split(" ");
+		List<String> ans = new ArrayList<>();
+		int idx = 0;
+		int len = parts.length;
+		while (idx < len) {
+			if (parts[idx].contains("='") && !parts[idx].endsWith("'")) {
+				StringBuilder builder = new StringBuilder(parts[idx++]);
+				while (idx < len) {
+					builder.append(" ").append(parts[idx]);
+					if (parts[idx++].endsWith("'")) {
+						break;
+					}
+				}
+				ans.add(builder.toString());
+			} else if (parts[idx].contains("=[") && !parts[idx].endsWith("]")) {
+				StringBuilder builder = new StringBuilder(parts[idx++]);
+				while (idx < len) {
+					builder.append(" ").append(parts[idx]);
+					if (parts[idx++].endsWith("]")) {
+						break;
+					}
+				}
+				ans.add(builder.toString());
+			} else {
+				String part = parts[idx++];
+				if (StringUtils.isNotEmpty(part)) {
+					ans.add(part);
 				}
 			}
-		} else {
-			Set<String> params = getSqlParams(config.getSqlManager().getSql(ctx.getSqlName()));
-			for (String key : params) {
-				console.putString(" >> param : " + key + "=");
-				console.flush();
-				String val = console.readLine();
+		}
+		return ans.toArray(new String[ans.size()]);
+	}
+
+	/**
+	 * SQLバインドパラメータを設定する
+	 *
+	 * @param ctx SQLコンテキスト
+	 * @param paramsArray パラメータ配列
+	 */
+	private void setSqlParams(final SqlContext ctx, final String[] paramsArray) {
+		for (String element : paramsArray) {
+			String[] param = element.split("=");
+			String key = param[0];
+			if (param.length == 1) {
+				// キーだけの指定は値をnullと扱う
+				ctx.param(key, null);
+			} else {
+				String val = param[1];
 				setParam(ctx, key, val);
 			}
 		}
@@ -433,6 +521,8 @@ public class SqlREPL {
 	 *  <dd><code>null</code>を設定する</dd>
 	 * 	<dh>[EMPTY]</dh>
 	 *  <dd>""（空文字）を設定する</dd>
+	 *  <dh>'値'</dh>
+	 *  <dd>文字列として設定する. 空白を含めることもできる</dd>
 	 * 	<dh>[値1,値2,...]</dh>
 	 *  <dd>配列として設定する</dd>
 	 * 	<dh>その他</dh>
@@ -453,22 +543,26 @@ public class SqlREPL {
 			}
 			ctx.paramList(key, vals);
 		} else {
-			if (StringUtils.isNotBlank(val)) {
-				ctx.param(key, convertSingleValue(val));
-			}
+			ctx.param(key, convertSingleValue(val));
 		}
 	}
 
 	/**
 	 * パラメータで渡された単独の値を型変換する
+	 *
 	 * @param val 値の文字列
 	 * @return 変換後オブジェクト
 	 */
 	private Object convertSingleValue(final String val) {
-		if ("[NULL]".equalsIgnoreCase(val)) {
+		if (StringUtils.isEmpty(val)) {
+			return null;
+		} else if ("[NULL]".equalsIgnoreCase(val)) {
 			return null;
 		} else if ("[EMPTY]".equalsIgnoreCase(val)) {
 			return "";
+		} else if (val.startsWith("'") && val.endsWith("'")) {
+			// ''で囲まれた値は文字列として扱う。空白を含むこともできる。 ex) 'This is a pen'
+			return val.substring(1, val.length() - 1);
 		} else if (Boolean.TRUE.toString().equalsIgnoreCase(val)) {
 			return Boolean.TRUE;
 		} else if (Boolean.FALSE.toString().equalsIgnoreCase(val)) {
@@ -501,6 +595,7 @@ public class SqlREPL {
 
 	/**
 	 * SQLの探索
+	 *
 	 * @param node SQLノード
 	 * @param params パラメータが見つかった場合に格納するSetオブジェクト
 	 */
@@ -530,6 +625,7 @@ public class SqlREPL {
 
 	/**
 	 * 評価式の探索
+	 *
 	 * @param node SQLノード
 	 * @param params パラメータが見つかった場合に格納するSetオブジェクト
 	 */
@@ -588,27 +684,23 @@ public class SqlREPL {
 		 */
 		@Override
 		public int complete(final String buffer, final int cursor, final List<CharSequence> candidates) {
-			String[] parts = buffer.split(" ");
+			String[] parts = getLineParts(buffer);
 			boolean isBlank = buffer.endsWith(" ");
 			int len = parts.length;
 			if (len == 1 || (len == 2 && !isBlank)) {
-				try {
-					if (Command.valueOf(parts[0].toLowerCase()).hasArgs()) {
-						// コマンドが引数ありの場合
-						String args = len == 2 ? parts[1] : "";
-						if (StringUtils.isEmpty(args)) {
-							candidates.addAll(sqlNames);
-						} else {
-							for (String match : sqlNames.tailSet(args)) {
-								if (!match.startsWith(args)) {
-									break;
-								}
-								candidates.add(match);
+				if (Command.toCommand(parts[0]).hasArgs()) {
+					// コマンドが引数ありの場合
+					String args = len == 2 ? parts[1] : "";
+					if (StringUtils.isEmpty(args)) {
+						candidates.addAll(sqlNames);
+					} else {
+						for (String match : sqlNames.tailSet(args)) {
+							if (!match.startsWith(args)) {
+								break;
 							}
+							candidates.add(match);
 						}
 					}
-				} catch (Exception ex) {
-					// do nothing
 				}
 				return candidates.isEmpty() ? buffer.length() : parts[0].length() + 1;
 			} else {
@@ -625,57 +717,53 @@ public class SqlREPL {
 		 */
 		@Override
 		public int complete(final String buffer, final int cursor, final List<CharSequence> candidates) {
-			String[] parts = buffer.split(" ");
+			String[] parts = getLineParts(buffer);
 			int pos = buffer.length();
 			int len = parts.length;
 			boolean isBlank = buffer.endsWith(" ");
 			if (len >= 2) {
-				try {
-					if (Command.valueOf(parts[0].toLowerCase()).hasArgs()) {
-						// sqlNameが指定されている場合
-						String sqlName = parts[1];
-						if (StringUtils.isNotEmpty(sqlName)) {
-							String sql = config.getSqlManager().getSql(sqlName);
-							if (StringUtils.isNotEmpty(sql)) {
-								Set<String> params = getSqlParams(sql);
-								if (len >= 3) {
-									// 最後のパラメータ以外ですでに指定されたバインドパラメータを候補から除去する
-									int lastPos = isBlank ? len : len - 1;
-									for (int i = 2; i < lastPos; i++) {
-										String part = parts[i];
-										String[] keyValue = part.split("=", 2);
-										if (keyValue.length == 2) {
-											params.remove(keyValue[0]);
-										}
+				if (Command.toCommand(parts[0]).hasArgs()) {
+					// sqlNameが指定されている場合
+					String sqlName = parts[1];
+					if (StringUtils.isNotEmpty(sqlName)) {
+						String sql = config.getSqlManager().getSql(sqlName);
+						if (StringUtils.isNotEmpty(sql)) {
+							Set<String> params = getSqlParams(sql);
+							if (len >= 3) {
+								// 最後のパラメータ以外ですでに指定されたバインドパラメータを候補から除去する
+								int lastPos = isBlank ? len : len - 1;
+								for (int i = 2; i < lastPos; i++) {
+									String part = parts[i];
+									String[] keyValue = part.split("=", 2);
+									if (keyValue.length == 2) {
+										params.remove(keyValue[0]);
 									}
-									if (isBlank) {
-										// 候補の表示位置を計算
-										candidates.addAll(params);
+								}
+								if (isBlank) {
+									// 候補の表示位置を計算
+									candidates.addAll(params);
+								} else {
+									// 候補の表示位置を計算
+									pos = pos - parts[len - 1].length();
+									// 最後のパラメータについて候補を作成
+									String[] keyValue = parts[len - 1].split("=", 2);
+									if (keyValue.length == 2) {
+										// すでに値の入力があるため補完は行わない
+										pos = -1;
 									} else {
-										// 候補の表示位置を計算
-										pos = pos - parts[len - 1].length();
-										// 最後のパラメータについて候補を作成
-										String[] keyValue = parts[len - 1].split("=", 2);
-										if (keyValue.length == 2) {
-											// すでに値の入力があるため補完は行わない
-											pos = -1;
-										} else {
-											String key = keyValue[0];
-											for (String match : params) {
-												if (match.startsWith(key)) {
-													candidates.add(match);
-												}
+										String key = keyValue[0];
+										for (String match : params) {
+											if (match.startsWith(key)) {
+												candidates.add(match);
 											}
 										}
 									}
-								} else {
-									candidates.addAll(params);
 								}
+							} else {
+								candidates.addAll(params);
 							}
 						}
 					}
-				} catch (Exception ex) {
-					// do nothing
 				}
 				return pos;
 			} else {
