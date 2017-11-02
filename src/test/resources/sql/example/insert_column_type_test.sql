aa5fb9536a22dc2adc2cd2393e10fd7336273cf5
Hideki Sugimoto <h.sugimoto.24@future.co.jp>
2017-11-02 17:46:14 +0900

    resolve #39 (#71)

---
+++ src/test/resources/sql/example/insert_column_type_test.sql
@@ -0,0 +1,10 @@
+INSERT INTO COLUMN_TYPE_TEST VALUES
+(
+	/*col_varchar*/'varchar'
+,	/*col_char*/'char'
+,	/*col_numeric*/123.45
+,	/*col_boolean*/true
+,	/*col_timestamp*/'2017-01-01T01:23:45'
+,	/*col_date*/'2017-01-01'
+,	/*col_time*/'01:23:45'
+)
