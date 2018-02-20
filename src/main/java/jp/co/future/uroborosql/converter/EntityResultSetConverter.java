8cdb77ede52c6806853951d580c016971fde2b74
Hideki Sugimoto <h.sugimoto.24@future.co.jp>
2018-02-20 12:56:58 +0900

    fix #106 Fix not to use ThreadLocal

--- src/main/java/jp/co/future/uroborosql/converter/EntityResultSetConverter.java
+++ src/main/java/jp/co/future/uroborosql/converter/EntityResultSetConverter.java
@@ -5,10 +5,8 @@ import java.lang.reflect.InvocationTargetException;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.util.Objects;
 
 import jp.co.future.uroborosql.exception.UroborosqlRuntimeException;
-import jp.co.future.uroborosql.mapping.JavaType;
 import jp.co.future.uroborosql.mapping.MappingColumn;
 import jp.co.future.uroborosql.mapping.MappingUtils;
 import jp.co.future.uroborosql.mapping.mapper.PropertyMapperManager;
@@ -27,40 +25,11 @@ public class EntityResultSetConverter<E> implements ResultSetConverter<E> {
 	/** ロガー */
 	private static final Logger LOG = LoggerFactory.getLogger(EntityResultSetConverter.class);
 
-	private class CacheMetaData {
-		private final ResultSet cacheResultSet;
-		private final int columnCount;
-		private final String[] columnLabels;
-
-		CacheMetaData(final ResultSet rs) throws SQLException {
-			this.cacheResultSet = rs;
-			ResultSetMetaData rsmd = rs.getMetaData();
-			this.columnCount = rsmd.getColumnCount();
-
-			this.columnLabels = new String[this.columnCount];
-			for (int i = 1; i <= this.columnCount; i++) {
-				this.columnLabels[i - 1] = rsmd.getColumnLabel(i);
-			}
-		}
-
-		boolean equalResultSet(final ResultSet rs) {
-			return Objects.equals(this.cacheResultSet, rs);
-		}
-
-		void bindValue(final E rec, final ResultSet rs, final MappingColumn column) throws SQLException {
-			for (int i = 0; i < this.columnLabels.length; i++) {
-				if (this.columnLabels[i].equalsIgnoreCase(column.getName())) {
-					column.setValue(rec, getValue(column.getJavaType(), rs, i + 1));
-					return;
-				}
-			}
-		}
-	}
-
 	private final PropertyMapperManager mapperManager;
 	private final Constructor<E> constructor;
 	private final MappingColumn[] columns;
-	private final ThreadLocal<CacheMetaData> cacheMetaData = new ThreadLocal<>();
+	private int columnCount;
+	private String[] columnLabels;
 
 	/**
 	 * コンストラクタ
@@ -80,10 +49,6 @@ public class EntityResultSetConverter<E> implements ResultSetConverter<E> {
 		this.columns = MappingUtils.getMappingColumns(entityType);
 	}
 
-	Object getValue(final JavaType type, final ResultSet rs, final int columnIndex) throws SQLException {
-		return this.mapperManager.getValue(type, rs, columnIndex);
-	}
-
 	/**
 	 * {@inheritDoc}
 	 *
@@ -91,17 +56,22 @@ public class EntityResultSetConverter<E> implements ResultSetConverter<E> {
 	 */
 	@Override
 	public E createRecord(final ResultSet rs) throws SQLException {
-		try {
-			CacheMetaData metaData = this.cacheMetaData.get();
-			if (metaData == null || !metaData.equalResultSet(rs)) {
-				metaData = new CacheMetaData(rs);
-				this.cacheMetaData.set(metaData);
-			}
-			E rec = this.constructor.newInstance();
-			for (MappingColumn column : this.columns) {
-				metaData.bindValue(rec, rs, column);
-			}
+		if (columnLabels == null) {
+			ResultSetMetaData rsmd = rs.getMetaData();
+			columnCount = rsmd.getColumnCount();
 
+			// columnLabelsは1始まりの配列で値を格納
+			columnLabels = new String[columnCount + 1];
+			for (int i = 1; i <= columnCount; i++) {
+				columnLabels[i] = rsmd.getColumnLabel(i);
+			}
+		}
+
+		try {
+			E rec = constructor.newInstance();
+			for (MappingColumn column : columns) {
+				bindValue(rec, rs, column);
+			}
 			return rec;
 		} catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
 			LOG.error("Error!!", e);
@@ -111,4 +81,14 @@ public class EntityResultSetConverter<E> implements ResultSetConverter<E> {
 			throw e;
 		}
 	}
+
+	private void bindValue(final E rec, final ResultSet rs, final MappingColumn column) throws SQLException {
+		for (int i = 1; i <= columnCount; i++) {
+			if (columnLabels[i].equalsIgnoreCase(column.getName())) {
+				column.setValue(rec, mapperManager.getValue(column.getJavaType(), rs, i));
+				return;
+			}
+		}
+	}
+
 }
