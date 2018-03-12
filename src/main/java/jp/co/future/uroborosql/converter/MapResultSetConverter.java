53bedee48d0259e3fe44f6a67e6196d3224211a6
Hideki Sugimoto <h.sugimoto.24@future.co.jp>
2018-03-13 03:01:29 +0900

    fix #110 (#113)
    The target column type is as follows
    java.sql.Clob -> String
    java.sql.NClob -> String
    java.sql.Blob -> byte[]
    java.sql.Array -> Object[]
    
    In the case of java.sql.Array, it should be a type obtained by getBaseType(), but since Java class mapped to JDBCType can not be specified, Object[] is used.

--- src/main/java/jp/co/future/uroborosql/converter/MapResultSetConverter.java
+++ src/main/java/jp/co/future/uroborosql/converter/MapResultSetConverter.java
@@ -1,5 +1,10 @@
 package jp.co.future.uroborosql.converter;
 
+import java.sql.Array;
+import java.sql.Blob;
+import java.sql.Clob;
+import java.sql.JDBCType;
+import java.sql.NClob;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
@@ -15,9 +20,13 @@ import jp.co.future.uroborosql.utils.CaseFormat;
  *
  */
 public class MapResultSetConverter implements ResultSetConverter<Map<String, Object>> {
-	private CaseFormat caseFormat = CaseFormat.UPPER_SNAKE_CASE;
+	private CaseFormat caseFormat;
 
+	/**
+	 * コンストラクタ
+	 */
 	public MapResultSetConverter() {
+		this(CaseFormat.UPPER_SNAKE_CASE);
 	}
 
 	/**
@@ -40,9 +49,31 @@ public class MapResultSetConverter implements ResultSetConverter<Map<String, Obj
 		int columnCount = rsmd.getColumnCount();
 		Map<String, Object> record = new LinkedHashMap<>(columnCount);
 		for (int i = 1; i <= columnCount; i++) {
-			record.put(caseFormat.convert(rsmd.getColumnLabel(i)), rs.getObject(i));
+			record.put(caseFormat.convert(rsmd.getColumnLabel(i)), getValue(rs, rsmd, i));
 		}
 		return record;
+	}
+
+	private Object getValue(final ResultSet rs, final ResultSetMetaData rsmd, final int columnIndex)
+			throws SQLException {
+		JDBCType type = JDBCType.valueOf(rsmd.getColumnType(columnIndex));
+		switch (type) {
+		case CLOB:
+			Clob clob = rs.getClob(columnIndex);
+			return clob.getSubString(1, (int) clob.length());
+		case NCLOB:
+			NClob nclob = rs.getNClob(columnIndex);
+			return nclob.getSubString(1, (int) nclob.length());
+		case BLOB:
+			Blob blob = rs.getBlob(columnIndex);
+			return blob.getBytes(1, (int) blob.length());
+		case ARRAY:
+			Array arr = rs.getArray(columnIndex);
+			return arr.getArray();
+		default:
+			return rs.getObject(columnIndex);
+		}
+
 	}
 
 }
