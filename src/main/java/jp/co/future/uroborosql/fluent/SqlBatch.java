6932412c6172396a5559f589d40217b61ff8f9a8
Hideki Sugimoto <h.sugimoto.24@future.co.jp>
2018-04-14 23:28:46 +0900

    add license header
    - Added copyright holder to LICENSE file.
    - Added license-maven-plugin configuration to pom.xml.
    - Added license header comment to java file under src/main folder.

--- src/main/java/jp/co/future/uroborosql/fluent/SqlBatch.java
+++ src/main/java/jp/co/future/uroborosql/fluent/SqlBatch.java
@@ -1,4 +1,10 @@
 /**
+ * Copyright (c) 2017-present, Future Corporation
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+/**
  *
  */
 package jp.co.future.uroborosql.fluent;
