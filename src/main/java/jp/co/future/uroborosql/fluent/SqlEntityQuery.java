cfeadba2984311dbfb5f14f5cf3dd372c25c3163
Hideki Sugimoto <h.sugimoto.24@future.co.jp>
2018-04-15 21:13:50 +0900

    add license header (#123)
    * add license header
    
    - Added copyright holder to LICENSE file.
    - Added license-maven-plugin configuration to pom.xml.
    - Added license header comment to java file under src/main folder.
    
    * add license header

--- src/main/java/jp/co/future/uroborosql/fluent/SqlEntityQuery.java
+++ src/main/java/jp/co/future/uroborosql/fluent/SqlEntityQuery.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2017-present, Future Corporation
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 package jp.co.future.uroborosql.fluent;
 
 import java.util.List;
