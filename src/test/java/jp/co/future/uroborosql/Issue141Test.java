4a703287f1125e508417ce6add4758c6cb332833
ota <y.ohta.z3@future.co.jp>
2019-01-10 15:36:02 +0900

    Fixed an issue where `.queryWith("...").stream().map(r -> r).iterator()` caused an error.
    Fixed #141

---
+++ src/test/java/jp/co/future/uroborosql/Issue141Test.java
@@ -0,0 +1,139 @@
+package jp.co.future.uroborosql;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.DriverManager;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import jp.co.future.uroborosql.config.SqlConfig;
+
+public class Issue141Test {
+	/** ロガー */
+	private static final Logger log = LoggerFactory.getLogger(Issue141Test.class);
+
+	private SqlConfig config;
+
+	private SqlAgent agent;
+
+	@Before
+	public void setUp() throws Exception {
+		config = UroboroSQL.builder(DriverManager.getConnection("jdbc:h2:mem:SqlAgentTest")).build();
+		config.getSqlAgentFactory().setFetchSize(1000);
+		agent = config.agent();
+		String[] sqls = new String(Files.readAllBytes(Paths.get("src/test/resources/sql/ddl/create_tables.sql")),
+				StandardCharsets.UTF_8).split(";");
+		for (String sql : sqls) {
+			if (StringUtils.isNotBlank(sql)) {
+				agent.updateWith(sql.trim()).count();
+			}
+		}
+		agent.commit();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		agent.close();
+	}
+
+	private List<Map<String, Object>> getDataFromFile(final Path path) {
+		List<Map<String, Object>> ans = new ArrayList<>();
+		try {
+			Files.readAllLines(path, StandardCharsets.UTF_8).forEach(line -> {
+				Map<String, Object> row = new LinkedHashMap<>();
+				String[] parts = line.split("\t");
+				for (String part : parts) {
+					String[] keyValue = part.split(":", 2);
+					row.put(keyValue[0].toLowerCase(), StringUtils.isBlank(keyValue[1]) ? null : keyValue[1]);
+				}
+				ans.add(row);
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return ans;
+	}
+
+	private void truncateTable(final Object... tables) {
+		Arrays.asList(tables).stream().forEach(tbl -> {
+			try {
+				agent.updateWith("truncate table " + tbl.toString()).count();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+				fail("TABLE:" + tbl + " truncate is miss. ex:" + ex.getMessage());
+			}
+		});
+	}
+
+	private void cleanInsert(final Path path) {
+		List<Map<String, Object>> dataList = getDataFromFile(path);
+
+		dataList.stream().map(map -> map.get("table")).collect(Collectors.toSet())
+		.forEach(tbl -> truncateTable(tbl));
+
+		dataList.forEach(map -> {
+			try {
+				agent.update(map.get("sql").toString()).paramMap(map).count();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+				fail("TABLE:" + map.get("TABLE") + " insert is miss. ex:" + ex.getMessage());
+			}
+		});
+	}
+
+	@Test
+	public void test() {
+		// 事前条件
+		cleanInsert(Paths.get("src/test/resources/data/setup", "testExecuteQuery.ltsv"));
+
+		// test
+		Iterator<?> itr = agent.queryWith("select product_id from product")
+				.stream()
+				.iterator();
+
+		int c = 0;
+		while (itr.hasNext()) {
+			itr.next();
+			c++;
+		}
+		assertThat(c, is(2));
+	}
+
+	@Test
+	public void testIssue() {
+		// 事前条件
+		cleanInsert(Paths.get("src/test/resources/data/setup", "testExecuteQuery.ltsv"));
+
+		// test
+		Iterator<?> itr = agent.queryWith("select product_id from product")
+				.stream()
+				.map(r -> r)
+				.iterator();
+
+		int c = 0;
+		while (itr.hasNext()) {
+			itr.next();
+			c++;
+		}
+		assertThat(c, is(2));
+	}
+}
